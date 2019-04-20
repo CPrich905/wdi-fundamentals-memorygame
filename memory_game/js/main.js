776e31d45793003596e0119410086b3d5ad1a298
Your Name <Home@Charless-MacBook-Pro.local>
2019-04-20 11:49:28 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,25 @@
 console.log("Up and running!");
 
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[2];//ID cardOne as queen
-cardsInPlay.push(cardOne);//adds cardOne to inPlay
-console.log("User flipped " + cardsInPlay);
 
-
-var cardTwo = cards[0];//ID cardTwo as king
-cardsInPlay.push(cardTwo);//adds cardTwo to inPlay
-console.log("User flipped " + cardsInPlay[1]);
-
-// add logic to see if two cards have been played
-
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again!")
+function checkForMatch() {
+	//checks cards match, prints result to console, not alert
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again!")
+		}
 	}
 }
 
-/*
-var cardThree = cards[1];//ID cardThree as queen
-var cardFour = cards[3];//ID cardFour as king
-*/
+function flipCard(cardId) {
+	//identify cards flipped & add to cardsInPlay
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	//runs previous function to see if cards match.
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
